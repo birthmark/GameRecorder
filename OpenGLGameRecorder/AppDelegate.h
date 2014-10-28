f56fee53fb56f2a38844b73341527df8ec848e5b
alankong <kongglin@163.com>
2014-10-28 17:10:12 +0800

    add code

---
+++ OpenGLGameRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLGameRecorder
+//
+//  Created by alankong on 10/28/14.
+//  Copyright (c) 2014 alankong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLGameRecorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenGLGameRecorder
+//
+//  Created by alankong on 10/28/14.
+//  Copyright (c) 2014 alankong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
